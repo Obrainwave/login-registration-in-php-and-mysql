8e12f8ac825b79470c801f1f0535a25addca262d
Olaiwola Akeem <olaiwolaakeem@gmail.com>
2020-05-19 17:43:21 +0100

    commits

--- forgot_form.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('gen_fns.php');
-do_html_header('Forgot Password');
-display_site_info();
-display_forgot_form();
-//do_html_footer();
-?>
--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('gen_fns.php');
-do_html_header('Login or Sign Up');
-display_site_info();
-display_login_form();
-//do_html_footer();
-?>
